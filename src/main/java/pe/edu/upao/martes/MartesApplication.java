c9cbe725716eaa0401a4ba239c3d37f1e6b92589
Ali Lozano <alilozanoc@gmail.com>
2023-08-29 08:08:36 -0500

    first commit

---
+++ src/main/java/pe/edu/upao/martes/MartesApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.martes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upao/martes/MartesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.martes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
